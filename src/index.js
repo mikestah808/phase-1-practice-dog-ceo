4ae227a6fc363473abafd3b16b630fd8e21314bd
Michael Stafford <michaelstafford808@gmail.com>
2022-03-04 12:14:59 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+//CHALLEGE 1
+
+window.addEventListener('DOMContentLoaded', () => {
+    getDogs();
+    getBreed();
+})
+
+
+
+function getDogs(){
+    const div = document.getElementById('dog-image-container')
+    fetch(imgUrl)
+    .then(res => res.json())
+    .then(data => {
+        data.message.forEach(dog =>{
+
+            let img = document.createElement('img')
+            img.src = dog
+            div.append(img)
+        })
+    })
+}
+
+//CHALLEGE 2
+
+function getBreed(){
+const ul = document.getElementById('dog-breeds')
+fetch(breedUrl)
+.then(res => res.json())
+.then(data => {
+    data.message.forEach(breed => {
+        let li = document.createElement('li')
+        li.innerText = breed
+        ul.append(li)
+    })
+})
+}
