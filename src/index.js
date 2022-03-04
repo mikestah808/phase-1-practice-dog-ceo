f96a7e488ccfac8a820461c1da452fa19f1b0d89
Michael Stafford <michaelstafford808@gmail.com>
2022-03-04 12:01:52 -0700

    commit tree 48f2fc9b68aeeeb513ec45f463f36dd4aed6e310

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 console.log('%c HI', 'color: firebrick')
+
+//CHALLEGE 1
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+window.addEventListener('DOMContentLoaded', () => {
+    getDogs();
+})
+
+
+
+function getDogs(){
+    const div = document.getElementById('dog-image-container')
+    fetch(imgUrl)
+    .then(res => res.json())
+    .then(data => {
+        data.message.forEach(dog =>{
+
+            let img = document.createElement('img')
+            img.src = dog
+            div.append(img)
+        })
+    })
+}
+
+//CHALLEGE 2
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+const ul = document.getElementById('dog-breeds')
+fetch(breedUrl)
+.then(res => res.json())
+.then(data => {
+    
+})
